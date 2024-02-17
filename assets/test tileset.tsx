698f8a47dfc37c44f240bafcf36d492ed7d4a3b7
Blaede Court-Suzuki <bcourtsu@ucsc.edu>
2024-02-16 17:53:03 -0800

    first commit

---
+++ assets/test tileset.tsx
@@ -0,0 +1,344 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="test tileset" tilewidth="16" tileheight="16" tilecount="160" columns="40">
+ <image source="testtileset.png" width="640" height="64"/>
+ <tile id="10">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="cancollide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="cancollide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
